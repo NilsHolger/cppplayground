b81b6742465e5118330ddbf54ed1b4da0f7fdd84
NilsHolger <nils@nilsnaegele.com>
2016-10-16 10:04:50 +0200

    var init clean up

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -7,17 +7,13 @@
 using namespace std;
 
 int main(){
-	size_t num_ss, total_length;
-	vector<string> postfixes;
-	string s1, appended;
-	s1 = "she has a sexy string"; 
-	num_ss = count(begin(s1), end(s1), 's');
-	postfixes = { "1", "2", "3" };
-	total_length = 0;
+	const string s1 = "she has a sexy string"; 
+	const auto num_ss = count(begin(s1), end(s1), 's');
+	const vector<string> postfixes = { "1", "2", "3" };
+	int total_length = 0;
 	for (const auto &postfix : postfixes){
-		appended = s1 + postfix;
+		const auto appended = s1 + postfix;
 		total_length += appended.size();
 	}
-	cout << "count s " << num_ss << "\n"; //4
-	cout << "total length " << total_length << "\n"; //66
+	cout << "count s " << num_ss << "\n" << "total length " << total_length << "\n"; // 4 66
 }
