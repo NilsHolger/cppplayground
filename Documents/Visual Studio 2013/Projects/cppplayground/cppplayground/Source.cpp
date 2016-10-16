54f85c86d7659350152f673ea9b56b36586df8e1
NilsHolger <nils@nilsnaegele.com>
2016-10-16 17:06:14 +0200

    template func, ptr intf

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -21,5 +21,7 @@ void print_elements(const T &items){
 
 int main(){
 	array<int, 3> ints{ { 3, 2, 1 } };
-	print_elements(ints);
+	array<string, 6> strings{ { "she", "has", "the", "sexiest", "string", "!" } };
+	array<char, 3> chars{ { 'a', 'b', 'c' } };
+	print_elements(chars);
 }							 
