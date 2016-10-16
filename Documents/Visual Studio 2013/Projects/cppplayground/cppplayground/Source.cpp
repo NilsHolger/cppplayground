c3e45230260324e87a22e779eb8a60d0f6e5dbb0
NilsHolger <nils@nilsnaegele.com>
2016-10-16 10:44:54 +0200

    c++ 11 const lambda iife

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -6,15 +6,17 @@
 #include "geometry.h"
 using namespace std;
 
-struct S_Counter {
-	S_Counter(string t_s):
-		s(move(t_s)),
-		num_ss(count(s.begin(), s.end(), 's')) {}
-	const string s;
-	const int num_ss;
-};
+string multichoiceinit(int in){
+	const string s1 = [&](){
+		switch (in){
+		case 0: return "0 Arguments";
+		case 1: return "1 Argument";
+		default: return "Many Arguments";
+		}
+	}();
+	return s1;
+}
 
 int main(){
-	const string s1 = "she has a sexy string"; 
-	cout << S_Counter(s1).num_ss << "\t" << S_Counter(s1).s << "\n";
+	cout << multichoiceinit(1) << "\n";
 }
