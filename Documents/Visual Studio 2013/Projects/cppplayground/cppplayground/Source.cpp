a44fe6ea5a3733ddf0e4af21e6a0488ee39bf6fe
NilsHolger <nils@nilsnaegele.com>
2016-10-16 09:56:13 +0200

    init

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -6,20 +6,18 @@
 #include "geometry.h"
 using namespace std;
 
-
 int main(){
-	vector<int> v1, v2;
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_int_distribution<> dis(1, 100);
-	generate_n(back_inserter(v1), 100, [&]() { return dis(gen); });
-	generate_n(back_inserter(v2), 100, [&]() { return dis(gen); });
-	vector<int> in_both;
-	sort(begin(v1), end(v1));
-	sort(begin(v2), end(v2));
-	set_intersection(begin(v1), end(v1), begin(v2), end(v2), back_inserter(in_both));
-	for (const auto i : in_both){
-		cout << i << ", ";
+	size_t num_ss, total_length;
+	vector<string> postfixes;
+	string s1, appended;
+	s1 = "she has a sexy string"; 
+	num_ss = count(begin(s1), end(s1), 's');
+	postfixes = { "1", "2", "3" };
+	total_length = 0;
+	for (const auto &postfix : postfixes){
+		appended = s1 + postfix;
+		total_length += appended.size();
 	}
-	cout << "\n";
+	cout << "count s " << num_ss << "\n"; //4
+	cout << "total length " << total_length << "\n"; //66
 }
