7ee017321dfd94d2ed824447bd5fcc8ffb2f5658
NilsHolger <nils@nilsnaegele.com>
2016-10-16 16:57:52 +0200

    fix pointer interface

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -9,35 +9,17 @@
 #include "windows.h"
 #include <cmath>
 #include <memory>
+#include <array>
 using namespace std;
 
-struct Derived {
-	int do_something(){
-		return value++;
+template<typename T>
+void print_elements(const T &items){
+	for (const auto &e : items){
+		cout << e << "\n";
 	}
-	int value = 10;
-};
-struct Derived2 {
-	int do_something(){
-		return value += 1;
-	}
-	int value = 100;
-};
+}
 
 int main(){
-	vector<Derived> objs1;
-	vector<Derived2> objs2;
-
-	for (int i = 0; i < 1000; ++i){
-		objs1.emplace_back();
-		objs2.emplace_back();
-	}
-	const auto do_something = [](auto &v){
-		for (auto &o : v){
-			o.do_something();
-		}
-	};
-
-	do_something(objs1);
-	do_something(objs2);
-}
+	array<int, 3> ints{ { 3, 2, 1 } };
+	print_elements(ints);
+}							 
