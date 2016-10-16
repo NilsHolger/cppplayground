e6b60e4c3cf6c5169ed43508239b4957ba6cd822
NilsHolger <nils@nilsnaegele.com>
2016-10-16 13:23:22 +0200

    raii

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -4,26 +4,28 @@
 #include <algorithm>
 #include <random>
 #include <sstream>
+#include <chrono>
 #include "geometry.h"
+#include "windows.h"
 using namespace std;
 
-class Particle {
-public:
-	Particle(int id, string description) : m_id(id), m_description(move(description)) {}
-	string description() const { return m_description; }
-private:
-	int m_id = 0;
-	string m_description;
-	int m_x_velocity = 0;
-	int m_y_velocity = 0;
-};
-
-int main(){
-	vector<Particle> particles;
-	for (int i = 0; i < 10; ++i){
-		particles.emplace_back(i, "ID: " + to_string(i));
+struct Timer {
+	const chrono::time_point<chrono::system_clock> start = chrono::system_clock::now();
+	~Timer(){
+		cout << chrono::duration<double>{chrono::system_clock::now() - start}.count() << "s\n";
 	}
-	for (const auto &p : particles){
-		cout << "Particle: " << p.description() << "\n";
+};
+bool complex_calculation() {
+	//perform a complex calculation here
+	Sleep(500);
+	return true;
+}
+int main(){
+	Timer t;
+	if (complex_calculation()){
+		return EXIT_SUCCESS;
+	}
+	else {
+		return EXIT_FAILURE;
 	}
 }
