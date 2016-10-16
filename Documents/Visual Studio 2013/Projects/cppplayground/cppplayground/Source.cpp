9d7e8deafa60996c0f41dafff437f9c93e3a3662
NilsHolger <nils@nilsnaegele.com>
2016-10-16 16:11:46 +0200

    stack-based

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -10,39 +10,34 @@
 #include <cmath>
 #include <memory>
 using namespace std;
-struct Base {
-	virtual int do_something() = 0;
-	virtual ~Base() = default;
-	Base() = default;
-	//Base(Base &&) = default;
-	Base(const Base &) = default;
-	//Base& operator = (Base &&) = default;
-	Base& operator = (const Base &) = default;
-};
 
-struct Derived : Base {
-	virtual int do_something() override {
+struct Derived {
+	int do_something(){
 		return value++;
 	}
 	int value = 10;
 };
-
-struct Derived2 : Base {
-	virtual int do_something() override {
+struct Derived2 {
+	int do_something(){
 		return value += 1;
 	}
 	int value = 100;
 };
 
 int main(){
-		
-	vector<shared_ptr<Base>> objs;
+	vector<Derived> objs1;
+	vector<Derived2> objs2;
 
-	for (int i = 0; i < 1000; i++){
-		objs.push_back(make_shared<Derived>());
-		objs.push_back(make_shared<Derived2>());
+	for (int i = 0; i < 1000; ++i){
+		objs1.emplace_back();
+		objs2.emplace_back();
 	}
-	for (const auto &o : objs){
-		cout << o->do_something() << "\n";
-	}
+	const auto do_something = [](auto &v){
+		for (auto &o : v){
+			o.do_something();
+		}
+	};
+
+	do_something(objs1);
+	do_something(objs2);
 }
