b5d600ef7503803c2134a5eceb638c50ff1cd570
NilsHolger <nils@nilsnaegele.com>
2016-10-19 21:33:26 +0200

    generic class

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -3,14 +3,25 @@
 #include <sstream>
 #include <vector>
 using namespace std;
-template <typename T>
-bool equals(T value1, T value2){
-	return (value1 == value2) ? true : false;
-}
+template<typename T>
+class List {
+private:
+	vector<T> datastore;
+public:
+	List() {}
+	~List() {}
+	void add(T item){
+		datastore.push_back(item);
+	}
+	void display(){
+		for (int i = 0; i < datastore.size(); ++i){
+			cout << datastore[i] << "\n";
+		}
+	}
+};
 int main(){
-	int a = 0, b = 1;
-	cout << equals(a, b) << "\n";
-	string s1, s2;
-	s1 = s2 = "you sexy string";
-	cout << equals(s1, s2) << "\n";
-}
+	List<string> tL; tL.add("eat"); tL.add("sleep"); tL.add("code"); tL.add("repeat");
+	tL.display();
+	List<int> in; in.add(1); in.add(2); in.add(3); in.add(4); in.add(5); in.add(6);
+	in.display();
+}	
