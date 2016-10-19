2a4c8caabac562d86fd8610bf23fc5e7ffcd7e5c
NilsHolger <nils@nilsnaegele.com>
2016-10-19 20:34:37 +0200

    func tmpl with multi data types

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -4,63 +4,13 @@
 #include <vector>
 using namespace std;
 
-template<typename T>
-class Stack {
-private:
-	T datastore[100];
-	int top;
-public:
-	Stack(){
-		top = -1;
-	}
-	~Stack(){}
-	void push(T num){
-		++top;
-		datastore[top] = num;
-	}
-	T pop(){
-		T value = datastore[top];
-		datastore[top] = 0;
-		--top;
-		return value;
-	}
-	T peek(){
-		return datastore[top];
-	}
-};
-template<>
-class Stack<string> {
-private:
-	string datastore[100];
-	int top;
-public:
-	Stack(){
-		top = -1;
-	}
-	~Stack(){}
-	void push(string val){
-		++top;
-		datastore[top] = val;
-	}
-	string pop(){
-		string value = datastore[top];
-		datastore[top] = "";
-		--top;
-		return value;
-	}
-	string peek(){
-		return datastore[top];
-	}
-};
-
+template <typename T, typename U>
+T max(T arg1, U arg2){
+	return (arg1 > arg2) ? arg1 : arg2;
+}
 int main(){
-	Stack<int> numbers;
-	numbers.push(10); numbers.push(20); numbers.push(30);
-	cout << numbers.peek() << "\n"; //LIFO 30
-	cout << numbers.pop() << "\n"; //30
-	cout << numbers.peek() << "\n"; //20
-	Stack<string> strings; strings.push("Marie"); strings.push("Maria"); strings.push("Eva");
-	cout << strings.peek() << "\n"; //Eva
-	cout << strings.pop() << "\n"; //Eva
-	cout << strings.peek() << "\n"; //Maria
+	double num1 = 11.01;
+	int num2 = 10;
+	cout << max(num2, num1) << "\n";
+	//loss of data if data type first arg < second arg, truncation
 }
