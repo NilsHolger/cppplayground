6126825eec61bb970dded8af2ecd6cc0276950a6
NilsHolger <nils@nilsnaegele.com>
2016-10-19 21:44:37 +0200

    exceptions

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -3,25 +3,21 @@
 #include <sstream>
 #include <vector>
 using namespace std;
-template<typename T>
-class List {
-private:
-	vector<T> datastore;
-public:
-	List() {}
-	~List() {}
-	void add(T item){
-		datastore.push_back(item);
-	}
-	void display(){
-		for (int i = 0; i < datastore.size(); ++i){
-			cout << datastore[i] << "\n";
+
+int main(){
+	const int DivideByZero = 1;
+	try	{
+		int num = 10; int den = 0;
+		if (den == 0){
+			throw DivideByZero;
+		}
+		else {
+			cout << num / den << "\n";
 		}
 	}
-};
-int main(){
-	List<string> tL; tL.add("eat"); tL.add("sleep"); tL.add("code"); tL.add("repeat");
-	tL.display();
-	List<int> in; in.add(1); in.add(2); in.add(3); in.add(4); in.add(5); in.add(6);
-	in.display();
+	catch (int e){
+		if (e == DivideByZero){
+			cout << "can't divide by zero" << "\n";
+		}
+	}
 }	
