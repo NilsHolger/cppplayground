3f916fb6ad0c0564bfae15c14ba223c47a1021ef
NilsHolger <nils@nilsnaegele.com>
2016-10-20 21:32:08 +0200

    runtime polymorphism

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -12,11 +12,12 @@
 using namespace std;
 
 int main(){
-	pair<int, string> n(1, "Node");
-	cout << n.first << " " << n.second << "\n";
-	map<int, string> o; o[1] = "JS"; o[2] = "C++"; o[3] = "V8"; o[4] = "C#";
-	cout << o[1] << " " << o.size() << "\n"; o.erase(4); cout << o.size() << "\n";
-	int p = 3; if (o.find(p) != o.end()){ cout << o[p] << "\n"; }
-	map<int, string>::iterator it = o.begin();
-	while (it != o.end()){ cout << it->second << "\n"; it++; }
+		
+	try {
+		throw runtime_error("error occured");
+	}
+	catch (const exception &e){
+		cout << e.what() << "\n";
+	}
+
 }	
