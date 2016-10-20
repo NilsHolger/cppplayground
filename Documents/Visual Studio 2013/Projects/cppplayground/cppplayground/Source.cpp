872e09ea4d5b3683b7f76d6b38c54018b62efd84
NilsHolger <nils@nilsnaegele.com>
2016-10-20 21:43:27 +0200

    basic inheritance

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -10,14 +10,18 @@
 #include <utility>
 #include <map>
 using namespace std;
-
+struct Base {
+	Base(const int i) : m_i(i){}
+	void do_something()	{ m_i += 4; }
+	int m_i = 0;
+};
+struct Derived : Base{
+	Derived() : Base(10) {}
+	void do_something_else() { ++m_i; }
+};
 int main(){
-		
-	try {
-		throw runtime_error("error occured");
-	}
-	catch (const exception &e){
-		cout << e.what() << "\n";
-	}
-
+	Derived d;
+	d.do_something();
+	d.do_something_else();
+	cout << d.m_i << '\n'; //15
 }	
