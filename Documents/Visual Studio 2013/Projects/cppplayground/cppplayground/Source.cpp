409a545761b96ee162e969445e8b290e5888aa67
NilsHolger <nils@nilsnaegele.com>
2016-10-21 21:45:58 +0200

    know standard library

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 int main(){
-	const vector<int> v{ 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1 };
-	const vector<string> w{ "you", "sexy", "string" };
-	const vector<char> x{ 'h','e','l','l','o','w','o','r','l','d' };
-	//for (auto itr = v.begin(); itr != v.end(); ++itr){
-	//	cout << *itr << "\n";
-	//}
-	for (const auto &i : x){
-		cout << i << "\n";
-	}
+	const vector<int> v{ 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 2 };
+	const bool found = any_of(begin(v), end(v), [](int i){return i > 1; });
+	cout << found << "\n";
 }	
