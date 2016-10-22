708600eb339ebec7a375ded56289ce48606aa16b
NilsHolger <nils@nilsnaegele.com>
2016-10-22 12:59:20 +0200

    uniform init

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -6,18 +6,41 @@
 #include <iterator>
 #include <cstring>
 #include <chrono>
-#include <windows.h>
+#include <map>
 using namespace std;
+struct Description {
 
-struct Timer {
-	~Timer(){
-		cout << chrono::duration<double>{chrono::system_clock::now() - start}
-		.count() << "s\n";
-	}
-	chrono::time_point<chrono::system_clock> start = chrono::system_clock::now();
 };
-
+struct Technologies {
+	int id;
+	string name;
+	Description description;
+	Technologies(const int id, const string &name) : id(id), name(name) {}
+	Technologies(const Description &description) : description(description){}
+};
+struct Exchange{
+	int count;
+	float rates[2];
+	Exchange(initializer_list<float> r){
+		if (r.size() < 2){ return; }
+		auto i = r.begin();
+		rates[0] = *i; i++;
+		rates[1] = *i;
+	}
+};
 int main(){
-	Timer t;
-	Sleep(100);
+	Exchange e{ 1.01f, 1.001f };
+	//uniform initialization
+	int n{ 1 };
+	string s{ "you sexy string" };
+	vector<int> v{ 1, 2, 3, 4, 5, 6 };
+	map<int, string> c{
+		{ 1, "Germany" },
+		{ 2, "USA" },
+		{ 3, "France" }
+	};
+	Technologies t{ 1, "JavaScript" };
+	cout << t.name << '\n';
+
+
 }
