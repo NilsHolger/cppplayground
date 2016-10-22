781d416884456b25ba721b91c2b06c8350d4cc33
NilsHolger <nils@nilsnaegele.com>
2016-10-22 13:54:11 +0200

    virtual

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -10,25 +10,29 @@
 #include <cmath>
 #include <typeinfo>
 using namespace std;
-auto meaning_of_life() -> int{
-	return 1;
-}
-template<typename T, typename U>
-auto add(T t, U u) -> decltype(t + u){
-	return t + u;
-}
-auto get_fun(int a) ->double(*)(double){
-	switch (a){
-	case 1: return fabs;
-	case 2: return sin;
-	case 3: return cos;
+class Animal {
+protected:
+	int legs = 4;
+public:
+	Animal(int legs) : legs(legs)	{}
+	virtual int Walk(int steps){
+		return legs * steps * 10;
 	}
-}
+};
+class Human : public Animal {
+public:
+	Human() : Animal(2){}
+	int Walk(int steps) override {
+		return legs * steps * 20;
+	}
+};
+
+
 int main(){
-	auto a = 1 + 2; cout << typeid(a).name() << '\n';
-	auto b = add(1, 1.1); cout << typeid(b).name() << '\n';
-	auto c = { 1, 2 }; cout << typeid(c).name() << '\n';
-	auto d = [](int x) { return x * x; };
-	auto my_fun = get_fun(1); cout << typeid(my_fun).name() << '\n';
-	cout << my_fun(3) << '\n';
+	Animal a(5);
+	cout << a.Walk(10) << '\n';
+	Human b;
+	cout << b.Walk(10) << '\n';
+
+
 }
