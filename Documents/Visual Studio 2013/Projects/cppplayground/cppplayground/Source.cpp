ddd93a0935e101ccdc72c79fc6c565be3d5e6ccc
NilsHolger <nils@nilsnaegele.com>
2016-10-22 15:36:40 +0200

    static_assert

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -1,24 +1,18 @@
 #include "headers.h"
 using namespace std;
-enum class color {
-	red, green, blue, rainbow
-};
-int main(){
-	vector<int> v{ 1, 2, 3, 4, 5 };
-	auto res = 1;
-	for (int a : { 1, 2, 3, 4, 5, 6 }){
-		res *= a;
-	}
-	cout << res << '\n';
-	map<int, string> future; 
-	future.insert(pair<int,string>(1, "JavaScript"));
-	future.insert(pair<int, string>(2, "C++"));
-	future.insert(pair<int, string>(3, "NodeJS"));
-	future.insert(pair<int, string>(4, "V8"));
-	future.insert(pair<int, string>(5, "ms"));
-	future.erase(5);
-	for (const auto& kvp : future){
-		cout << kvp.first << " " << kvp.second << '\n';
-	}
 
+template<typename T, size_t Size>
+class Values {
+
+	static_assert(Size > 1, "use a scalar");
+	T values[Size];
+};
+template<typename T, typename U>
+auto multiply(T t, U u) -> int { 
+	static_assert(is_integral<T>::value, "first val must be an int");
+	return t * u;
+}
+int main(){
+	Values<string, 2> v; 
+	cout << multiply(10,10) << '\n';
 }
