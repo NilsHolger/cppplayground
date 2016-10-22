3028bd919ba258f96dea1066aff3be6f2d257320
NilsHolger <nils@nilsnaegele.com>
2016-10-22 07:32:24 +0200

    use standard algorithms

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -2,10 +2,21 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-
+#include <random>
+#include <iterator>
 using namespace std;
 int main(){
-	const vector<int> v{ 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 2 };
-	const bool found = any_of(begin(v), end(v), [](int i){return i > 1; });
-	cout << found << "\n";
-}	
+	vector<int> n1; vector<int> n2;
+	random_device rd; mt19937 gen(rd());
+	uniform_int_distribution<> dis(1, 100);
+	generate_n(back_inserter(n1), 100, [&]() {return dis(gen);});
+	generate_n(back_inserter(n2), 100, [&]() {return dis(gen);});
+	vector<int> in_both;
+	sort(n1.begin(), n1.end());
+	sort(n2.begin(), n2.end());
+	set_intersection(begin(n1), end(n1), begin(n2), end(n2), back_inserter(in_both));
+	for (const auto i : in_both){
+		cout << i << ',';
+	}
+	cout << '\n';
+}
