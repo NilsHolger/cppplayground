d114b3c0226ba852e7fee3d845812fbeea011239
NilsHolger <nils@nilsnaegele.com>
2016-10-22 14:30:52 +0200

    lambda

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -9,30 +9,18 @@
 #include <map>
 #include <cmath>
 #include <typeinfo>
+#include <functional>
 using namespace std;
-class Animal {
-protected:
-	int legs = 4;
-public:
-	Animal(int legs) : legs(legs)	{}
-	virtual int Walk(int steps){
-		return legs * steps * 10;
-	}
-};
-class Human : public Animal {
-public:
-	Human() : Animal(2){}
-	int Walk(int steps) override {
-		return legs * steps * 20;
-	}
-};
-
 
 int main(){
-	Animal a(5);
-	cout << a.Walk(10) << '\n';
-	Human b;
-	cout << b.Walk(10) << '\n';
-
-
+	vector<int> scores{ 10, 5, 1, 7, 9, 11 };
+	for_each(begin(scores), end(scores), [](int n){cout << n << '\n'; });
+	auto one = 1;
+	auto is_positive_tier = [=](int n){ return n > one; };
+	cout << boolalpha <<  is_positive_tier(2) << '\n';
+	function<int(int)> fib = [&fib](int x){return x < 2 ? 1 : fib(x - 1) + fib(x - 2); };
+	cout << fib(10) << '\n';
+	for (int i = 1; i <= 10; ++i){
+		cout << fib(i) << '\n';
+	}
 }
