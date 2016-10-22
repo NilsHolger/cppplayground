acfca7c616f405896f4ada9e9a9cde7e861a7a1b
NilsHolger <nils@nilsnaegele.com>
2016-10-22 09:58:07 +0200

    express state

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -4,24 +4,40 @@
 #include <algorithm>
 #include <random>
 #include <iterator>
+#include <cstring>
 using namespace std;
 
-class Technology {
-public:
-	Technology(int id, string description) : m_id(id), m_description(move(description)){}
-	string description() const { return m_description; }
+struct Vowel_Count {
+	Vowel_Count(string t_word, const int t_num_vowels)
+		:word(move(t_word)),num_vowels(t_num_vowels)  {}
+
+	int get_count() const {
+		++cache_calls;
+		return num_vowels;
+	}
+	string word;
 private:
-	int m_id = 0;
-	string m_description;
+	int num_vowels;
+	mutable int cache_calls = 0;
 };
+bool is_vowel(const char c){
+	vector<char> vowels = { 'a', 'e', 'i', 'o', 'u' };
+	return any_of(begin(vowels), end(vowels), [&](const char v) { return c == v; });
+}
+int count_vowels(string t_str){
+	
+	return count_if(begin(t_str), end(t_str), &is_vowel);
+}
 
 int main(){
-	vector<Technology> ts;
-	const vector<string> vs{ "C++", "JS", "NodeJS", "V8", "lib_uv" };
-	for (int i = 0; i < vs.size(); ++i){
-		ts.emplace_back(i, "ID: " + vs[i]);
+	vector<string> s1 = { "she", "has", "a", "sexy", "string" };
+	vector<Vowel_Count> vowel_counts;
+	for (auto itr : s1){
+		transform(begin(itr), end(itr), begin(itr), [](const char c){return tolower(c); });
+		const auto vowel_count = count_vowels(itr);
+		vowel_counts.emplace_back(move(itr), vowel_count);
 	}
-	for (const auto &t : ts){
-		cout << "T: " << t.description() << '\n';
+	for (const auto &vc : vowel_counts){
+		cout << vc.word << ": " << vc.get_count() << '\n';
 	}
 }
