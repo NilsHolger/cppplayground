07e75c6deabb9084457b09f8a80b986c0492a7dd
NilsHolger <nils@nilsnaegele.com>
2016-10-22 09:17:51 +0200

    obj init

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -6,11 +6,22 @@
 #include <iterator>
 using namespace std;
 
-int main(int argc, const char*[]){
-	const auto vc = [](){
-		vector<int> v(100, 0);
-		v[10] = 1;
-		return v;
-	}();
-	cout << vc[10] << '\n';
+class Technology {
+public:
+	Technology(int id, string description) : m_id(id), m_description(move(description)){}
+	string description() const { return m_description; }
+private:
+	int m_id = 0;
+	string m_description;
+};
+
+int main(){
+	vector<Technology> ts;
+	const vector<string> vs{ "C++", "JS", "NodeJS", "V8", "lib_uv" };
+	for (int i = 0; i < vs.size(); ++i){
+		ts.emplace_back(i, "ID: " + vs[i]);
+	}
+	for (const auto &t : ts){
+		cout << "T: " << t.description() << '\n';
+	}
 }
