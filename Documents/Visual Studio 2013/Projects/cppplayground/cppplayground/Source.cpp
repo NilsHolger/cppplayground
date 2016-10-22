8a0878f19ebd5c8897b04fcb804dab0177d2f9fd
NilsHolger <nils@nilsnaegele.com>
2016-10-22 17:41:27 +0200

    rvalues

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -1,40 +1,30 @@
 #include "headers.h"
 using namespace std;
 
-class Technology {
-public:
-	Technology(){};
-	Technology(const Technology&) { cout << "technology copied" << "\n"; }
-	int id;
-	double power;
-	string description;
-};
-
-class Software {
-public:
-	shared_ptr<Technology> technology;
-	Software(){
-		technology = make_unique<Technology>();
-	}
-};
-
-unique_ptr<Technology> create_technology_smart(int id, string description){
-	auto t = make_unique<Technology>();
-	t->id = id;
-	t->description = description;
-	return t;
+string getName(){
+	return "Node";
+}
+void printName(const string& name){
+	cout << "stand ref: " << name << '\n';
+}
+void printName(string&& name){
+	cout << "rvalue ref: " << name << '\n';
 }
 
 int main(){
-	auto mts = create_technology_smart(1, "01000101");
-	cout << mts->id << " " << mts->description << '\n';
-	shared_ptr<Technology> t;
-	{
-		Software s;
-		s.technology->id = 100;
-		s.technology->description = "11110011";
-		s.technology->power = 100;
-		t = s.technology;
-	}
-	cout << t->power << '\n';
+	
+	string name1 = getName(); cout << name1 << '\n';
+
+	string& name2 = getName(); cout << name2 << '\n';
+	name2 = "V8"; cout << name2 << '\n';
+
+	string&& name3 = getName();
+	name3 = "lib_uv"; cout << name3 << '\n';
+
+	string name4{ "JavaScript" };
+	printName(getName());
+	printName(name4);
+	string name5{ "C++" };
+	printName(getName());
+	printName(name5);
 }
