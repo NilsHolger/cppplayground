a2770cb2e4f9fb1bc16830a90c630b94b2a315a2
NilsHolger <nils@nilsnaegele.com>
2016-10-28 22:09:01 +0200

    iteration

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/countfind.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/countfind.cpp
@@ -1,26 +1,18 @@
 #include "headers.h"
 using namespace std;
 
-
-enum Color { Red, Green, Blue, Rainbow};
-enum class GameState : short { Win = 1, Loose, Live, Die};
-
-union Data
-{
-	int integer;
-	float fNumber;
-	char* text;
-};
-
-struct Size
-{
-	int width, height;
-};
-
 auto main() -> int 
 {
-	Size s{ 11, 10 };
-	cout << s.height << '\n';
+	int a[]	{1, 2, 3, 4, 5};
+	for (int i = 0; i < 5; ++i){cout << a[i] << '\n';}
+	for (int *p = a, *e = a + 5; p != e; ++p){cout << *p << '\n';}						   
+	auto ba = begin(a);auto ea = end(a);
+	for (; ba != ea; ba++){cout << *ba << '\n';}
+	for (auto av : a){cout << av << '\n';}
+	vector<int> v{ 6, 7, 8, 9, 10 };
+	auto bv = v.begin(); //begin(v);
+	auto cbv = v.cbegin(); //const itr
+	for (auto i = v.rbegin(); i != v.rend(); ++i){cout << *i << '\n';}
 	getchar();
 }
 
