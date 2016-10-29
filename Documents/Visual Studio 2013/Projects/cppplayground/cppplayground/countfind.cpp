05ddc43698066220b757b1b3e0aeb27dc4e97a2d
NilsHolger <nils@nilsnaegele.com>
2016-10-29 07:32:54 +0200

    switch bool conv

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/countfind.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/countfind.cpp
@@ -3,16 +3,31 @@ using namespace std;
 
 auto main() -> int 
 {
-	int a[]	{1, 2, 3, 4, 5};
-	for (int i = 0; i < 5; ++i){cout << a[i] << '\n';}
-	for (int *p = a, *e = a + 5; p != e; ++p){cout << *p << '\n';}						   
-	auto ba = begin(a);auto ea = end(a);
-	for (; ba != ea; ba++){cout << *ba << '\n';}
-	for (auto av : a){cout << av << '\n';}
-	vector<int> v{ 6, 7, 8, 9, 10 };
-	auto bv = v.begin(); //begin(v);
-	auto cbv = v.cbegin(); //const itr
-	for (auto i = v.rbegin(); i != v.rend(); ++i){cout << *i << '\n';}
+	char *s = "playground";
+	while (*s)
+	{
+		putchar(*s);
+		s++;
+	}
+
+	int *p = nullptr;
+	if (p){
+		cout << "do something" << '\n';
+	}
+
+	int i = 1;
+	switch (i)
+	{
+	case 0:
+		cout << "hello" << '\n';
+		break;
+	case 1:
+		cout << "world" << '\n';
+		break;
+	default:
+		break;
+	}
+
 	getchar();
 }
 
