a723f672722544025912c49d336bcffcf3a26a55
NilsHolger <nils@nilsnaegele.com>
2016-10-29 12:48:52 +0200

    move semantics rvalues

--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/Source.cpp
@@ -1,11 +1,93 @@
 #include "headers.h"
 using namespace std;
 
+class Address
+{
+public:
+	string country;
+	Address(){}
+	Address(string const& country) : country(country)
+	{
+		cout << "address created" << '\n';
+	}
+	~Address()
+	{
+		cout << "address destroyed" << '\n';
+	}
+};
+class Person
+{
+public:
+	Address *address;
+	Person(string const& country)
+	{
+		address = new Address(country);
+		cout << "person created" << '\n';
+	}
+	~Person()
+	{
+		delete address;
+		cout << "person destroyed" << '\n';
+	}
+};
+class Person2
+{
+	Person2() {}
+	Person2(const Person2& _) {}
+	~Person2() {}
 
-//int main() {
-//	vector<int> v{ 1, 2, 3 };
-//
-//	for_each(v.begin(), v.end(), [](int n){cout << n << '\n'; });
-//
-//
-//}
+	int ref_count;
+public:
+	static Person2* create()
+	{
+		auto *p = new Person2();
+		p->ref_count = 1;
+		return p;
+	}
+	void add_ref() { ref_count++; }
+	void release()
+	{
+		if (--ref_count == 0)
+		{
+			delete this;
+		}
+	}
+};
+void inc(int& x){ x++; }
+
+string meaningoflife() { return "progress"; }
+
+void move_rvalue()
+{
+	/*int a = 0;
+	inc(a);
+	int&& x = meaningoflife();
+	cout << x << '\n';*/
+
+	Address book[100];
+	book[1] = move(Address("Berlin"));
+
+	auto swap = [](int& a, int& b)
+	{
+		int tmp{ a };
+		a = b;
+		b = tmp;
+	};
+	//move semantics better memory allocation
+	auto betterswap = [](int& a, int& b)
+	{
+		int tmp(move(a));
+		a = move(b);
+		b = tmp;
+	};
+
+	int x = 10, y = 100;
+	//betterswap(x, y);
+	//cout << x << ": " << y << '\n';
+}
+
+auto main() -> int 
+{
+	move_rvalue();
+	getchar();
+}
--- Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/countfind.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/cppplayground/countfind.cpp
@@ -5,66 +5,66 @@
 #include "compfactory.h"
 using namespace std;
 
-auto main() -> int 
-{
-	Technology t(80, "t", Technology::on);   
-	//Technology *t2 = new Technology(50, "t2", Technology::off);
-
-	//t2->salute();
-	//int v = t.getViability();
-	//cout << v << '\n';
-
-	//Technology *t3 = new Technology(70, "t3", Technology::off);
-	//auto* address = new Address(100, "https", "www");
-	//t3->address = address;
-	//t3->salute();
-
-	//Technology t4(*t3);
-	//t3->address->url = "http";
-	//cout << t4.address->url << '\n';
-
-	Component c(90, "c", Technology::on,"a c c");
-
-	auto d = [](const Technology& t)
-	{
-		cout << t.name << '\n';
-	};
-	d(c);
-
-	Technology &tr = c;
-	//Component& cr = static_cast<Component&>(tr);
-	//cout << cr.description << '\n';
-
-	Technology& tr2 = t;
-	//access violation reading location, static_cast performs no checks
-	//Component& cr2 = static_cast<Component&>(tr2);
-	//cout << cr2.description << '\n';
-
-	tr.salute();
-	try
-	{
-		Component& cr2 = dynamic_cast<Component&>(tr2);
-		cout << cr2.description << '\n';
-	}
-	catch (const bad_cast& e)
-	{
-		cout << "cannot cast this " << e.what() << '\n';
-	}
-
-	Technology *tt = &t;
-	Component *cp = dynamic_cast<Component*>(tt);
-	if (cp){
-		cout << cp->description << '\n';
-	}
-	else {
-		cout << "failed to cast pointer " << cp << '\n';
-	}
-
-
-	//delete t2;
-	//delete t3;
-	getchar();
-}
+//auto main() -> int 
+//{
+//	Technology t(80, "t", Technology::on);   
+//	//Technology *t2 = new Technology(50, "t2", Technology::off);
+//
+//	//t2->salute();
+//	//int v = t.getViability();
+//	//cout << v << '\n';
+//
+//	//Technology *t3 = new Technology(70, "t3", Technology::off);
+//	//auto* address = new Address(100, "https", "www");
+//	//t3->address = address;
+//	//t3->salute();
+//
+//	//Technology t4(*t3);
+//	//t3->address->url = "http";
+//	//cout << t4.address->url << '\n';
+//
+//	Component c(90, "c", Technology::on,"a c c");
+//
+//	auto d = [](const Technology& t)
+//	{
+//		cout << t.name << '\n';
+//	};
+//	d(c);
+//
+//	Technology &tr = c;
+//	//Component& cr = static_cast<Component&>(tr);
+//	//cout << cr.description << '\n';
+//
+//	Technology& tr2 = t;
+//	//access violation reading location, static_cast performs no checks
+//	//Component& cr2 = static_cast<Component&>(tr2);
+//	//cout << cr2.description << '\n';
+//
+//	tr.salute();
+//	try
+//	{
+//		Component& cr2 = dynamic_cast<Component&>(tr2);
+//		cout << cr2.description << '\n';
+//	}
+//	catch (const bad_cast& e)
+//	{
+//		cout << "cannot cast this " << e.what() << '\n';
+//	}
+//
+//	Technology *tt = &t;
+//	Component *cp = dynamic_cast<Component*>(tt);
+//	if (cp){
+//		cout << cp->description << '\n';
+//	}
+//	else {
+//		cout << "failed to cast pointer " << cp << '\n';
+//	}
+//
+//
+//	//delete t2;
+//	//delete t3;
+//	getchar();
+//}
 
 
 
