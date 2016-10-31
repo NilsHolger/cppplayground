b9599e715677f3e12ac82d2752dc9a15b7843188
NilsHolger <nils@nilsnaegele.com>
2016-10-31 21:52:18 +0100

    guid

--- Documents/Visual Studio 2013/Projects/cppplayground/com/app1.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/com/app1.cpp
@@ -18,30 +18,25 @@ inline void HR(HRESULT const hr)
 	}
 }
 
-auto main() -> int
-{
-	try {
-		long hres = -1;
-		HR(hres);
-	}
-	catch (ComException e)
-	{
-		e.error;
-	}
-
-	HRESULT hr = S_OK;
-
-	if (S_OK == hr)
-	{
-		TRACE(L"succeeded\n");
-	}
-	else
-	{
-		TRACE(L"failed\n");
-	}
-
-
-
-
-
-}
+//auto main() -> int
+//{
+//	try {
+//		long hres = -1;
+//		HR(hres);
+//	}
+//	catch (ComException e)
+//	{
+//		e.error;
+//	}
+//
+//	HRESULT hr = S_OK;
+//
+//	if (S_OK == hr)
+//	{
+//		TRACE(L"succeeded\n");
+//	}
+//	else
+//	{
+//		TRACE(L"failed\n");
+//	}
+//}
