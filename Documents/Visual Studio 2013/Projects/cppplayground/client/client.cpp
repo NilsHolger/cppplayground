c1aac39147322f71dca6b41e8b5030de4e0c849f
NilsHolger <nils@nilsnaegele.com>
2016-11-01 11:11:32 +0100

    com client

---
+++ Documents/Visual Studio 2013/Projects/cppplayground/client/client.cpp
@@ -0,0 +1,41 @@
+#include "precompiled.h"
+
+using namespace std;
+using namespace Microsoft::WRL;
+
+struct ComException
+{
+	HRESULT result;
+	explicit ComException(HRESULT const value) :
+		result(value)
+	{
+		
+	}
+};
+inline void HR(HRESULT const result)
+{
+	_ASSERTE(S_OK == result);
+	if (S_OK != result)
+	{
+		throw ComException(result);
+	}
+}
+struct ComRuntime
+{
+	ComRuntime()
+	{
+		HR(CoInitializeEx(nullptr, COINIT_MULTITHREADED));
+	}
+	~ComRuntime()
+	{
+		CoUninitialize();
+	}
+};
+
+
+
+auto main() -> int
+{
+	ComRuntime runtime;
+
+}
