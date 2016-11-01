1c0b1ec8b95eff74ad7cadc230b0b63217f5857b
NilsHolger <nils@nilsnaegele.com>
2016-11-01 07:05:39 +0100

    smart pointer

--- Documents/Visual Studio 2013/Projects/cppplayground/com/application.cpp
+++ Documents/Visual Studio 2013/Projects/cppplayground/com/application.cpp
@@ -1,35 +1,60 @@
 #include "library.h"
+#include "precompiled.h"
 
+template<typename T>
+class RemoveAddRefRelease : public T
+{
+	ULONG __stdcall AddRef();
+	ULONG __stdcall Release();
+};
 
+template <typename T>
+class ComPtr
+{
+	T * m_ptr;
+public:
+	ComPtr() : m_ptr(0){}
+	~ComPtr()
+	{
+		if (m_ptr)
+		{
+			m_ptr->Release();
+		}
+	}
+	RemoveAddRefRelease<T> * operator->() const
+	{
+		ASSERT(m_ptr);
+		return static_cast<RemoveAddRefRelease<T> *>(m_ptr);
+	}
+	T ** GetAddressOf()
+	{
+		ASSERT(!m_ptr);
+		return &m_ptr;
+	}
+};
 
 auto main() -> int
 {
-	ILion *lion;
+	ComPtr<ILion> lion;
 
-	if (S_OK != CreateLion(&lion))
+	if (S_OK != CreateLion(lion.GetAddressOf()))
 	{
 		return 0;
 	}
 	lion->Roar();
 	//lion->Attack();
 	
-	ILion2 * lion2;
+	ComPtr<ILion2> lion2;
 
-	if (S_OK == lion->QueryInterface(&lion2))
+	if (S_OK == lion->QueryInterface(lion2.GetAddressOf()))
 	{
 		lion2->Hunt();
-		lion2->Release();
 	}
 
-	IOfflineLion * offline;
+	ComPtr<IOfflineLion> offline;
 
-
-	if (S_OK == lion->QueryInterface(&offline))
+	if (S_OK == lion->QueryInterface(offline.GetAddressOf()))
 	{
 		offline->Save("filename");
-		offline->Release();
 	}
-
-
-	lion->Release();
 }
